3ed96685ae8cfab504e8cf67363ce7b16fdd93db
William Phan <wphan@live.ca>
2018-01-28 01:26:07 -0800

    try to put you in root

--- main.py
+++ main.py
@@ -1,104 +1,101 @@
-"""
-Python2.7 only.
+import os
 
-Usage:
-    source activate catalyst
-    python buy_hold_compare.py
-"""
-import json
-import pytz
-import datetime
+import plotly.graph_objs as go
+from plotly.offline import plot
+from grab_marketcap_data import get_market_data
 
-import portfolio_optimization
-import buy_hodl_compare
-import portfolio_equal_weights
+START_DATE = "20170601"
+END_DATE = "20180126"
 
-from catalyst.utils.run_algo import run_algorithm
+def analyze():
+    colors = [
+        "rgb(244, 67, 54)",
+        "rgb(233, 30, 99)",
+        "rgb(156, 39, 176)",
+        "rgb(63, 81, 181)",
+        "rgb(33, 150, 243)",
+        "rgb(0, 188, 212)",
+        "rgb(0, 150, 136)",
+        "rgb(76, 175, 80)",
+        "rgb(205, 220, 57)",
+        "rgb(255, 193, 7)",
+        "rgb(255, 87, 34)",
+        "rgb(96, 125, 139)",
+        "rgb(121, 85, 72)"
+    ]
 
-import global_vars
+    # gets a bunch of markets, normalize, and plot
+    coin_sets= [['bitcoin', 'ethereum', 'ripple', 'bitcoin-cash', 'cardano'],
+                ['bitcoin', 'stellar', 'litecoin', 'eos', 'neo'],
+                ['bitcoin', 'nem', 'iota', 'dash', 'monero'],
+                ['bitcoin', 'tron', 'vechain', 'bitcoin-gold', 'icon'],
+                ['bitcoin', 'qtum', 'ethereum-classic', 'lisk', 'raiblocks'],
+                ['bitcoin', 'populous', 'omisego', 'tether', 'steem'],
+                ['bitcoin', 'zcash', 'stratis', 'bytecoin-bcn', 'binance-coin'],
+                ['bitcoin', 'verge', 'siacoin', 'bitshares', '0x'],
+                ['bitcoin', 'status', 'ardor', 'walton', 'augur'],
+                ['bitcoin', 'maker', 'waves', 'dogecoin', 'veritaseum']]
+    col_to_plot = ["close", "volume", "market cap"]
+    line_style = ["line", "dash", "dot"]
+    for coin_set in coin_sets:
+        traces = []
+        for coin_idx, coin in enumerate(coin_set):
+            # normalize all numeric columns:
+            cols_to_norm = ['open', 'high', 'low', 'close', 'volume', 'market cap']
+            df = get_market_data(coin, START_DATE, END_DATE)
+            df = df.sort_index(ascending=False)
+            df[cols_to_norm] = df[cols_to_norm].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+
+            # create traces for each interesting column
+            for idx, col in enumerate(col_to_plot):
+                # create a noramlized trace
+                traces.append(go.Scatter(
+                    x=df['date'],
+                    y=df[col],
+                    mode="lines",
+                    name='_'.join([coin, col]),
+                    line=dict(
+                        color=colors[coin_idx],
+                        dash=line_style[idx],
+                        smoothing=1.2
+                    ),
+                    visible='legendonly'
+                ))
+
+        # plot
+        title = "Comparing Price, Volume, and Market Cap"
+        layout = go.Layout(
+            title=title,
+            xaxis=dict(
+                title='Date',
+                showticklabels=True,
+                tickangle=45
+            ),
+            yaxis=dict(
+                title='USD Value (normalized)',
+                showticklabels=True,
+                tickangle=45
+            )
+        )
+        fig = go.Figure(data=traces, layout=layout)
+        name = '_'.join(coin_set) + ".html"
+        plot(fig, filename=os.path.join("plots", name))
 
 
-def main():
-    with open("sectors.json", "r") as f:
-        sectors = json.load(f)
-    start = datetime.datetime(2017, 10, 26, 0, 0, 0, 0, pytz.utc)
-    end = datetime.datetime(2018, 1, 4, 0, 0, 0, 0, pytz.utc)
-
-    for sector in sectors:
-        # only run sectors with sufficient info
-        if sector in global_vars.ignored_sectors:
-            print("skipping {}".format(sector))
-            continue
-
-        # first run an optimized sector
-        global_vars.SECTOR_NAME = sector
-        print("\nRunning optimized portfolio for: {}".format(global_vars.SECTOR_NAME))
-        run_algorithm(initialize=portfolio_optimization.initialize,
-                      handle_data=portfolio_optimization.handle_data,
-                      analyze=portfolio_optimization.analyze,
-                      start=start,
-                      end=end,
-                      exchange_name='bittrex',
-                      capital_base=100000, )
-        print("\nDone optimized portfolio for: {}".format(global_vars.SECTOR_NAME))
-
-        # try with equal weights
-        global_vars.SECTOR_NAME = sector
-        print("\nRunning portfolio with equal weights for: {}".format(global_vars.SECTOR_NAME))
-        run_algorithm(initialize=portfolio_equal_weights.initialize,
-                      handle_data=portfolio_equal_weights.handle_data,
-                      analyze=portfolio_equal_weights.analyze,
-                      start=start,
-                      end=end,
-                      exchange_name='bittrex',
-                      capital_base=100000, )
-        print("\nDone portfolio with equal weights for: {}".format(global_vars.SECTOR_NAME))
-
-        # then run a buy and hodl for each asset in the sector
-        for asset in sectors[sector]:
-            global_vars.TARGET_ASSET = asset.replace("/", "_")
-            if global_vars.TARGET_ASSET in global_vars.ignored_assets:
-                print("{} ignored, too new?".format(global_vars.TARGET_ASSET))
-                continue
-
-            print("\n    Running buy and hodl comparisons for: {}".format(global_vars.TARGET_ASSET))
-            run_algorithm(initialize=buy_hodl_compare.initialize,
-                          handle_data=buy_hodl_compare.handle_data,
-                          analyze=buy_hodl_compare.analyze,
-                          start=start,
-                          end=end,
-                          exchange_name='bittrex',
-                          capital_base=100000, )
-
-
-def eq_only():
-    with open("sectors.json", "r") as f:
-        sectors = json.load(f)
-    start = datetime.datetime(2017, 10, 26, 0, 0, 0, 0, pytz.utc)
-    end = datetime.datetime(2018, 1, 4, 0, 0, 0, 0, pytz.utc)
-
-    for sector in sectors:
-        # only run sectors with sufficient info
-        if sector in global_vars.ignored_sectors:
-            print("skipping {}".format(sector))
-            continue
-
-        # first run an optimized sector
-        global_vars.SECTOR_NAME = sector
-        # try with equal weights
-        global_vars.SECTOR_NAME = sector
-        print("\nRunning portfolio with equal weights for: {}".format(global_vars.SECTOR_NAME))
-        run_algorithm(initialize=portfolio_equal_weights.initialize,
-                      handle_data=portfolio_equal_weights.handle_data,
-                      analyze=portfolio_equal_weights.analyze,
-                      start=start,
-                      end=end,
-                      exchange_name='bittrex',
-                      capital_base=100000, )
-        print("\nDone portfolio with equal weights for: {}".format(global_vars.SECTOR_NAME))
+def generate_index_html():
+    plots = os.listdir('plots')
+    with open("index.html", 'w') as f:
+        f.write("<!DOCTYPE HTML>\n")
+        f.write("<html>\n")
+        f.write("  <body>\n")
+        f.write("    <H1>Market Cap compared with Price and Volume</H1>\n")
+        for plot in plots:
+            link_name = plot.split('.')[0].replace('_', ' ')
+            f.write("    <a href='plots/{0}'>{1}</a><br>\n".format(plot, link_name))
+        f.write("  </body>\n")
+        f.write("</html>\n")
 
 
 if __name__ == "__main__":
-    main()
-    # eq_only()
-    # check_price_history()
+    analyze()
+    generate_index_html()
